0b60d7b214f868f93608ead4faa6ce7b9bdedd6c
Christoffer Wigforss <christoffer.wigforss@volvo.com>
2025-01-29 13:19:26 +0100

    Add support for specifying input and output file paths via CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use log::{error, info};
 use protobuf_to_zod::generator::zod_generator::generate_zod_schemas;
 use protobuf_to_zod::parser::parse_proto_file;
+use std::env;
 use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
@@ -8,9 +9,17 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let mut proto_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    proto_path.push("files");
-    proto_path.push("logdservice.proto");
+    // Get the paths to the Protobuf file and output file from the command line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!(
+            "Usage: {} <path_to_proto_file> <path_to_output_file>",
+            args[0]
+        );
+        std::process::exit(1);
+    }
+    let proto_path = PathBuf::from(&args[1]);
+    let output_path = PathBuf::from(&args[2]);
 
     info!("Reading Protobuf file from: {}", proto_path.display());
 
@@ -34,7 +43,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     info!("Parsed content: {:#?}", proto_file);
 
     let zod_schemas = generate_zod_schemas(&proto_file);
-    println!("Generated Zod schemas:\n{}", zod_schemas);
+    info!("Generated Zod schemas:\n{}", zod_schemas);
+
+    // Write the generated Zod schemas to the output file
+    fs::write(&output_path, zod_schemas).map_err(|e| {
+        error!("Failed to write to the output file: {}", e);
+        format!(
+            "Failed to write to the output file '{}': {}",
+            output_path.display(),
+            e
+        )
+    })?;
+
+    info!(
+        "Successfully wrote Zod schemas to: {}",
+        output_path.display()
+    );
 
     Ok(())
 }
